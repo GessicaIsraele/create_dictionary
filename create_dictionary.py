f09da15e136dd35847e167b75bb7c1547b49338a
Géssica Israele <gessicaisraele@hotmail.com>
2025-10-20 22:07:47 -0300

    Add function create_dictionary

--- create_dictionary.py
+++ create_dictionary.py
@@ -1,10 +1,9 @@
-def create_dictionary(*args):
+
+def create_dictionary(*args) -> dict:
     new_dict = {}
     for i, arg in enumerate(args):
-        if isinstance(arg, (int, float, str, bool, tuple, type(None))):
-            new_dict[arg] = i
-        elif callable(arg):
-            new_dict[arg] = i
-        else:
+        if isinstance(arg, (list, set, dict)):
             print(f"Cannot add {arg} to the dict!")
+        else:
+            new_dict[arg] = i
     return new_dict
